1301cab4b43428432b56a16c063b7e5fcf50c66f
MarioPalaciosLop <plopezmarioalfr@gmail.com>
2023-12-17 01:05:04 -0500

    add exceptions

--- src/main/java/com/provedores/api/converter/ProveedorConverter.java
+++ src/main/java/com/provedores/api/converter/ProveedorConverter.java
@@ -10,50 +10,6 @@ import com.provedores.api.entity.Sucursal;
 @Component
 public class ProveedorConverter extends AbstractConverter<GrupoProveedor, GruposProveedorDTO> {
 
-    // private int id;
-    // private String codigoGrupo;
-    // private String grupoDescripcion;
-    // private Empresa empresa;
-    // private Sucursal sucursal;
-    /*
-     * Cuerpo del API GruposProveedor:
-     * {
-     * "id": 93,
-     * "codigoGrupo": "1234556",
-     * "grupoDescripcion": "ABC",
-     * "empresa": {
-     * "id": 1,
-     * "nombre": "PRUEBA ACTUALIZACION 1",
-     * "nroDocumento": "EXAMPLE1",
-     * "razonSocial": "EXAMPLE1",
-     * "direccion": "EXAMPLE1",
-     * "activo": false,
-     * "createdAt": "2023-12-16T22:52:55.000+00:00",
-     * "updatedAt": "2023-12-16T23:05:01.000+00:00"
-     * },
-     * "activo": true,
-     * "sucursal": {
-     * "id": 10,
-     * "nombre": "PRUEBA",
-     * "empresa": {
-     * "id": 1,
-     * "nombre": "PRUEBA ACTUALIZACION 1",
-     * "nroDocumento": "EXAMPLE1",
-     * "razonSocial": "EXAMPLE1",
-     * "direccion": "EXAMPLE1",
-     * "activo": false,
-     * "createdAt": "2023-12-16T22:52:55.000+00:00",
-     * "updatedAt": "2023-12-16T23:05:01.000+00:00"
-     * },
-     * "direccion": "PRUEBA",
-     * "createdAt": "2023-12-17T03:00:43.000+00:00",
-     * "updatedAt": null
-     * },
-     * "createdAt": "2023-12-17T03:07:15.000+00:00",
-     * "updatedAt": null
-     * }
-     */
-
     @Override
     public GruposProveedorDTO fromEntity(GrupoProveedor entity) {
         if (entity == null)
